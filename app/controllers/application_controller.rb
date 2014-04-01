d28501bfa70f6aed064629a4674ba593bde805e3
Thermatix <mbeckerwork@gmail.com>
2014-04-01 12:43:08 +0100

    adds some css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :logged_in?
+
+  def logged_in?
+  	if (session[:token] && session[:time])
+  		if Time.now >= session[:time]
+			  returning = :old
+  		else
+        returning = :true 
+      end    		
+	  else
+	    returning = :false
+	  end
+	  returning
+  end
+
 end
